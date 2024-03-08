02b2d31ac95f7d99ba0b549fec6135a622f20e73
Yanis <mechtayanis20@gmail.com>
2024-03-09 00:50:21 +0100

    update classe

--- app/src/main/java/com/api/projet/entity/Anime.java
+++ app/src/main/java/com/api/projet/entity/Anime.java
@@ -1,2 +1,44 @@
-package com.api.projet.entity;public class Anime {
+package com.api.projet.entity;
+public class Anime {
+
+    private int id;
+
+    private String titre;
+
+    private String imageUri;
+
+    private int score;
+
+    private String status;
+
+    public Anime(int id, String titre, String imageUri, int score, String status){
+        this.id = id;
+        this.titre = titre;
+        this.imageUri = imageUri;
+        this.score = score;
+        this.status = status;
+    }
+    public int getId(){
+        return id;
+    }
+
+    public String getTitre(){
+        return titre;
+    }
+
+    public String getImageUri(){
+        return imageUri;
+    }
+
+    public int getScore(){
+        return score;
+    }
+
+    public String getStatus(){
+        return status;
+    }
+    @Override
+    public String toString(){
+        return " id : "+ this.id + " titre : "+ this.titre +" imageUri : "+ this.imageUri +" score : "+ this.score +" status : "+ this.status;
+    }
 }
