5268485ad03ac0f4117626bf2c42091485c0003e
Yanis <mechtayanis20@gmail.com>
2024-03-22 09:44:57 +0100

    ajout de l'entité lobby

---
+++ app/src/main/java/com/api/projet/entity/Lobby.java
@@ -0,0 +1,23 @@
+package com.api.projet.entity;
+
+public class Lobby {
+
+    public String name;
+
+    public String author;
+
+
+
+    public Lobby(String name, String author){
+        this.name = name;
+        this.author = author;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getAuthor(){
+        return this.author;
+    }
+}
